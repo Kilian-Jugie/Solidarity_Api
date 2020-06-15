f07235f14f89ef2e709d11146d5e95afd6635992
Kilian <kilian.jugie@viacesi.fr>
2020-06-15 18:30:17 +0200

    Doxygen comments and params specification

--- api/get/users.ts
+++ api/get/users.ts
@@ -2,7 +2,7 @@ import {APIRequest} from '../api';
 import { Request, Response } from 'express-serve-static-core';
 
 class GetUsers implements APIRequest {
-    execute(params: any, body: string, query: any, res: Response): void {
+    execute(params: String[], body: string, query: any, res: Response): void {
         res.send(query);
     }
 }
