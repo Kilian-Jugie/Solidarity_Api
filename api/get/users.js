40ede58bcd3f5669460bb660ef21b8a2137024a6
Kilian <kilian.jugie@viacesi.fr>
2020-06-19 22:44:16 +0200

    Removed user operation to respect REST

--- api/get/users.js
+++ api/get/users.js
@@ -3,9 +3,16 @@ var GetUsers = /** @class */ (function () {
     function GetUsers() {
     }
     GetUsers.prototype.execute = function (params, body, query, res, dbcon) {
-        dbcon.query("CALL Get_Users()", function (error, results, fields) {
-            res.send(results[0]);
-        });
+        if (params[1] != undefined) {
+            dbcon.query("CALL Get_User(?)", [+params[1]], function (error, results, fields) {
+                res.send(results[0][0]);
+            });
+        }
+        else {
+            dbcon.query("CALL Get_Users()", function (error, results, fields) {
+                res.send(results[0]);
+            });
+        }
         //res.send({"type": "answer", "users": ["kilian.jugie@viacesi.fr", "andreas.ducourneau@viacesi.fr", "thibaud.rapin@viacesi.fr"]});
     };
     return GetUsers;
--- api/get/users.ts
+++ api/get/users.ts
@@ -4,10 +4,16 @@ import { Connection } from 'mysql';
 
 class GetUsers implements APIRequest {
     execute(params: String[], body: any, query: any, res: Response, dbcon: Connection): void {
+        if(params[1] != undefined) {
+            dbcon.query("CALL Get_User(?)", [+params[1]], function(error, results, fields) {
+                res.send(results[0][0]);
+            });
+        }
+        else {
         dbcon.query("CALL Get_Users()", function(error, results, fields) {
             res.send(results[0]);
         });
-
+    }
 
         //res.send({"type": "answer", "users": ["kilian.jugie@viacesi.fr", "andreas.ducourneau@viacesi.fr", "thibaud.rapin@viacesi.fr"]});
     }
