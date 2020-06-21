efb4de8845e7f7daaf878637f854341694b88c8d
Kilian <kilian.jugie@viacesi.fr>
2020-06-21 19:34:27 +0200

    Fixed some things, message api

---
+++ api/get/messages.js
@@ -0,0 +1,16 @@
+"use strict";
+var GetMessages = /** @class */ (function () {
+    function GetMessages() {
+    }
+    GetMessages.prototype.execute = function (params, body, query, res, dbcon) {
+        if (params[1] == undefined || params[1] == "") {
+            res.status(404).send({ "type": "error", "description": "The account id is needed to see messages." });
+            return;
+        }
+        dbcon.query("CALL Get_Msg(?)", [+params[1]], function (error, results, fields) {
+            res.send(results[0]);
+        });
+    };
+    return GetMessages;
+}());
+module.exports = new GetMessages();
